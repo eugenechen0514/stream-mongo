4ff39e96fe38e9942f1596284371c1e820ca41d2
EugeneChen <yujiechen0514@gmail.com>
2019-11-14 15:25:00 +0800

    add status()

--- src/MongoCollectionMirrorSync.ts
+++ src/MongoCollectionMirrorSync.ts
@@ -1,10 +1,12 @@
+import {ChangeStream, MongoClient} from "mongodb";
+import {Writable} from "stream";
+import assert from 'assert';
+import event from 'events';
 import {
     cloneCollection,
     createLogChangeStreamTransform,
     mirrorChangeStreamToMongoDB
 } from "./index";
-import {ChangeStream, MongoClient} from "mongodb";
-import {Writable} from "stream";
 
 export interface MongoCollectionSyncOptions {
     sourceUrl: string,
@@ -14,18 +16,40 @@ export interface MongoCollectionSyncOptions {
     cloneBeforeWatch?: boolean,
 }
 
-export class MongoCollectionMirrorSync {
+enum MongoCollectionMirrorSyncEvent {
+    start = 'start',
+    stop = 'stop',
+}
+
+export class MongoCollectionMirrorSync extends event.EventEmitter {
     options: MongoCollectionSyncOptions;
+    sourceClient?: MongoClient;
+    targetClient?: MongoClient;
     sourceStream?: ChangeStream;
     targetStream?: Writable;
 
     constructor(options: MongoCollectionSyncOptions) {
-        this.options = Object.assign({cloneBeforeWatch: true}, options);
+        super();
+        this.options = Object.assign({cloneBeforeWatch: false}, options);
     }
 
     async __createChangeStream(sourceUrl: string, sourceCollection: string) {
         const mongoClient = await MongoClient.connect(sourceUrl);
-        return mongoClient.db().collection(sourceCollection).watch(undefined);
+        const changeStream = mongoClient.db().collection(sourceCollection).watch(undefined);
+
+        this.sourceClient = mongoClient;
+        this.sourceStream = changeStream;
+        return changeStream;
+    }
+
+    async __createWriteStream(targetUrl: string, targetCollection: string) {
+        const mongoClient = await MongoClient.connect(targetUrl);
+        const db = mongoClient.db();
+        const writable = mirrorChangeStreamToMongoDB({dbURL: targetUrl, collection: targetCollection, dbConnection: db});
+
+        this.targetClient = mongoClient;
+        this.targetStream = writable;
+        return writable;
     }
 
     async startWatch() {
@@ -40,24 +64,34 @@ export class MongoCollectionMirrorSync {
             console.log('Clone done');
         }
 
+        // Create clients
         const changeStream = await this.__createChangeStream(sourceUrl, sourceCollection);
-        this.sourceStream = changeStream;
+        const writable = await this.__createWriteStream(targetUrl, targetCollection);
 
         const transform = await createLogChangeStreamTransform();
-        const writable = mirrorChangeStreamToMongoDB({dbURL: targetUrl, collection: targetCollection});
-        this.targetStream = writable;
 
         changeStream.pipe(transform).pipe(writable);
-        console.log('start watching...')
+        console.log('start watching...');
+
+        this.emit(MongoCollectionMirrorSyncEvent.start);
 
         // TODO: Error handling: close/error/retry
     }
 
+
     async stopWatch() {
         try {
             this.sourceStream && await this.sourceStream.close();
+            this.emit(MongoCollectionMirrorSyncEvent.stop);
         } catch (e) {
             return e;
+        }
+    }
+
+    async status() {
+        return {
+            source: this.sourceClient && this.sourceClient.isConnected(),
+            target: this.targetClient && this.targetClient.isConnected(),
         }
     }
 }
--- tests/MongoCollectionMirrorSynctest.test.ts
+++ tests/MongoCollectionMirrorSynctest.test.ts
@@ -4,17 +4,15 @@ const expect = chai.expect;
 import MongoCollectionMirrorSync from "../src/MongoCollectionMirrorSync";
 
 describe('MongoCollectionMirrorSync', function () {
-    this.timeout(6000);
+    this.timeout(3000);
+    const sourceUrl = 'mongodb://eugene:1234@127.0.0.1/source';
+    const sourceCollection = 'SourceCollection';
+
+    const targetUrl = 'mongodb://eugene:1234@127.0.0.1/target';
+    const targetCollection = 'TargetCollection';
 
     it('start watch and then stop watch', (done) => {
-        const sourceUrl = 'mongodb://eugene:1234@127.0.0.1/source';
-        const sourceCollection = 'SourceCollection';
-
-        const targetUrl = 'mongodb://eugene:1234@127.0.0.1/target';
-        const targetCollection = 'TargetCollection';
-
         const sync = new MongoCollectionMirrorSync({
-            cloneBeforeWatch: true,
             sourceUrl,
             sourceCollection,
             targetUrl,
@@ -27,6 +25,77 @@ describe('MongoCollectionMirrorSync', function () {
         setTimeout(() => {
             sync.stopWatch();
             done();
-        }, 5000);
+        }, 1000);
+    });
+
+    it('when start watching, should get "start" event', (done) => {
+        let startEvent = false;
+        let stopEvent = false;
+
+        const sync = new MongoCollectionMirrorSync({
+            sourceUrl,
+            sourceCollection,
+            targetUrl,
+            targetCollection
+        });
+
+        sync.startWatch()
+            .catch(console.log);
+
+        sync.on('start', () => {
+            startEvent = true;
+        });
+
+        sync.stopWatch();
+
+        setTimeout(() => {
+            expect(startEvent).be.eq(true);
+            done();
+        }, 1000);
+    });
+
+    it('when start watching, status should be {source: true, target: true}', (done) => {
+
+        const sync = new MongoCollectionMirrorSync({
+            sourceUrl,
+            sourceCollection,
+            targetUrl,
+            targetCollection
+        });
+
+        sync.startWatch()
+            .catch(console.log);
+
+        setTimeout(async () => {
+            const status = await sync.status();
+            expect(status).be.deep.eq({source: true, target: true});
+            sync.stopWatch();
+            done();
+        }, 1000);
+    });
+
+    it('when stop watching, should get "stop" event', (done) => {
+        let stopEvent = false;
+
+        const sync = new MongoCollectionMirrorSync({
+            sourceUrl,
+            sourceCollection,
+            targetUrl,
+            targetCollection
+        });
+
+        sync.startWatch()
+            .catch(console.log);
+
+        sync.on('stop', () => {
+            stopEvent = true;
+        });
+
+        sync.stopWatch();
+
+        setTimeout(() => {
+            expect(stopEvent).be.eq(true);
+            done();
+        }, 1000);
     });
 });
